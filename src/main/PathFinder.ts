c55a5ff20078b272c7f24382825c9fed46d13388
Ziagl <office@ziegelwanger-edv.at>
2024-06-18 20:05:57 +0200

    implemented offset to cube coordinates conversion

--- src/main/PathFinder.ts
+++ src/main/PathFinder.ts
@@ -1,4 +1,4 @@
-import { Hex, offsetToCube } from "honeycomb-grid";
+import { CubeCoordinates, Hex, HexOffset, Orientation, defineHex, hexToOffset, offsetToCube } from "honeycomb-grid";
 import { Utils } from "./Utils";
 
 export class PathFinder
@@ -6,6 +6,10 @@ export class PathFinder
     private _map:number[][] = [];
     private _map_x:number = 0;
     //private _map_y:number = 0;
+
+    // TODO one should be able to change that config
+    private _offset:HexOffset = -1;
+    private _orientation:Orientation = Orientation.POINTY;
 
     public initialize(map:number[], rows:number, columns:number) {
         this._map = this.convertTo2DArray(map, rows, columns);
@@ -16,18 +20,35 @@ export class PathFinder
     }
 
     // computes path with lowest costs from start to end
-    public computePath(start:Hex, end:Hex):Hex[] {
-        let path:Hex[] = [];
+    public computePath(start:CubeCoordinates, end:CubeCoordinates):CubeCoordinates[] {
+        let path:CubeCoordinates[] = [];
 
         // TODO implement path finding
 
         return path;
     }
 
+    // additional computePath method for offset coordinates
     public computePathOffsetCoordinates(start:{x:number, y:number}, end:{x:number, y:number}):{x:number, y:number}[] {
         let path:{x:number, y:number}[] = [];
         
-        // TODO implement conversion
+        // convert offset coordinates to cube coordinates
+        const hexSetting = {offset: this._offset, orientation: this._orientation};
+        const startCube = offsetToCube(hexSetting, {col: start.x, row: start.y});
+        const endCube = offsetToCube(hexSetting, {col: end.x, row: end.y})
+
+        // compute path
+        const computedPath = this.computePath(startCube, endCube);
+
+        // convert resultin path back to offset coordinates
+        if(path.length > 0) {
+            const Hex = defineHex(hexSetting);
+            computedPath.forEach(coord => {
+                const hex = new Hex([coord.q, coord.r]);
+                const offset = hexToOffset(hex);
+                path.push({x:offset.row, y:offset.col});
+            });
+        }
 
         return path; 
     }
