ab416eb7d71f2bc23edc0fda6e67781d1b680052
Ziagl <office@ziegelwanger-edv.at>
2024-06-18 19:00:49 +0200

    fixtures for algorithm

--- src/main/PathFinder.ts
+++ src/main/PathFinder.ts
@@ -1,13 +1,35 @@
+import { Hex, offsetToCube } from "honeycomb-grid";
+import { Utils } from "./Utils";
+
 export class PathFinder
 {
     private _map:number[][] = [];
     private _map_x:number = 0;
     //private _map_y:number = 0;
 
-    public initialize(map:number[][], rows:number, columns:number) {
-        this._map = map;
+    public initialize(map:number[], rows:number, columns:number) {
+        this._map = this.convertTo2DArray(map, rows, columns);
         this._map_x = columns;
         //this._map_y = rows;
+
+        Utils.normalize(this._map);
+    }
+
+    // computes path with lowest costs from start to end
+    public computePath(start:Hex, end:Hex):Hex[] {
+        let path:Hex[] = [];
+
+        // TODO implement path finding
+
+        return path;
+    }
+
+    public computePathOffsetCoordinates(start:{x:number, y:number}, end:{x:number, y:number}):{x:number, y:number}[] {
+        let path:{x:number, y:number}[] = [];
+        
+        // TODO implement conversion
+
+        return path; 
     }
 
     // print map structured (one row as one line)
@@ -26,5 +48,13 @@ export class PathFinder
     // print map unstructured
     public print_unstructured() :string {
         return this._map.flat().join(" ");
+    }
+
+    private convertTo2DArray(map: number[], rows: number, cols: number): number[][] {
+        const twoDArray: number[][] = [];
+        for (let i = 0; i < rows; i++) {
+            twoDArray[i] = map.slice(i * cols, (i + 1) * cols);
+        }
+        return twoDArray;
     }
 };
--- src/test/PathFinder.test.ts
+++ src/test/PathFinder.test.ts
@@ -1,12 +1,7 @@
 import { PathFinder } from "../main/PathFinder";
 
 test('initialize', () => {
-    const exampleMap:number [][] = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ];      
+    const exampleMap:number [] = Array(16).fill(0);      
     const pf = new PathFinder();
     pf.initialize(exampleMap, 4, 4);
     const result = pf.print_unstructured();
